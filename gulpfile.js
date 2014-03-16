ecbc89922bfee4d3525f6e485673e172079f2a75
Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>
2014-03-16 21:51:48 +0100

    chore(coverage): add coverage in jasmine task

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@
 
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
+var istanbul = require('gulp-istanbul');
 var log = require('gulp-util').log;
 
 var jshint = require('gulp-jshint');
 
-var codeFiles = ['**/*.js', '!node_modules/**'];
+var codeFiles = ['serpa.js', 'spec/serpa-spec.js'];
 
 gulp.task('lint', function() {
   log('Linting Files');
@@ -15,9 +16,15 @@ gulp.task('lint', function() {
     .pipe(jshint.reporter());
 });
 
-gulp.task('jasmine', function() {
-    gulp.src(codeFiles)
-        .pipe(jasmine());
+gulp.task('jasmine', function(cb) {
+  gulp.src(codeFiles)
+    .pipe(istanbul()) // Covering files
+    .on('end', function () {
+        gulp.src('spec/serpa-spec.js')
+        .pipe(jasmine())
+        .pipe(istanbul.writeReports()) // Creating the reports after tests runned
+        .on('end', cb);
+    });
 });
 
 gulp.task('watch', function() {
